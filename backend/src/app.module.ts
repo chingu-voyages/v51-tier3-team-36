1da9575877ea310c706517f0dfa8d7dbeaf4e4e4
Hayvid <davidhyppolitedh@gmail.com>
2024-09-20 17:09:40 -0400

    dependecies an nest modules added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ExpensesModule } from './expenses/expenses.module';
+import { GroupsModule } from './groups/groups.module';
+import { ReceiptsModule } from './receipts/receipts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule, 
+    ExpensesModule, 
+    GroupsModule, 
+    ReceiptsModule, 
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/auth/schemas/auth.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types, HydratedDocument } from 'mongoose';
+
+export type AuthDocument = HydratedDocument<Expense>;
+
+@Schema()
+export class Expense {
+  @Prop({required: true})
+  name: string;
+
+  @Prop({required: true})
+  amount: number;
+
+  @Prop({required: true})
+  date: Date;
+
+  // association
+  @Prop({})
+  creator: Types.ObjectId;
+
+  @Prop()
+  group: Types.ObjectId;
+
+  @Prop({default: []})
+  expenses: Types.ObjectId[];
+}
+
+export const ExpenseSchema = SchemaFactory.createForClass(Expense);
