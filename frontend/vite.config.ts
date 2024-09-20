185a4125bb99ac5cf46759282480c9edbdafea7b
GaganSingh5 <gagansingh0797@gmail.com>
2024-09-20 16:58:48 -0400

    intial frontend commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()]
+      }
+    }
+  }
+})
