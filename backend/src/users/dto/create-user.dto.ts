9d5a21884873ef2f0fe27df187e3d2990d4acb9d
Hayvid <davidhyppolitedh@gmail.com>
2024-09-21 09:10:18 -0400

    code: create-user-dto validation

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,7 +1,6 @@
-import { IsString, IsEmail, IsOptional, isString } from "class-validator";
+import { IsString, IsEmail, IsOptional } from "class-validator";
 
 export class CreateUserDto {
-
     @IsString()
     name: string;
 
