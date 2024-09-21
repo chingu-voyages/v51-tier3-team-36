3056556321578d4cda0e96dab456d59de70f83e6
Hayvid <davidhyppolitedh@gmail.com>
2024-09-21 08:34:38 -0400

    Added swagger
    Co-authored-by: Gagandeep Singh <gagansingh0797@gmail.com>
    Co-authored-by: Richard Zhang <richardzhiyuanzhang@gmail.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: ['http://localhost:3000','http://localhost:5173'],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Expense Splitter')
+    .setDescription('Users and groups can handle expenses')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
