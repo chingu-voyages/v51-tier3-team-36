bc414180063f372e67bc8dcde534f2b5bcef2978
Hayvid <davidhyppolitedh@gmail.com>
2024-09-26 19:51:20 -0400

    added api property, and password requirements

--- backend/src/auth/dto/google-auth.dto.ts
+++ backend/src/auth/dto/google-auth.dto.ts
@@ -1,14 +1,25 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GoogleUserDto {
+  @ApiProperty({
+    description: 'Google name',
+  })
+  
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Google email',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Google ID',
+  })
   @IsString()
   @IsNotEmpty()
   googleId: string;
