791074dac789bda94e8efbc48737e55c209cbe2c
Hayvid <davidhyppolitedh@gmail.com>
2024-09-28 16:40:19 -0400

    added api property

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, IsEmail, IsOptional } from "class-validator";
 
 export class CreateUserDto {
+    @ApiProperty({description: 'User name'})
     @IsString()
     name: string;
 
+    @ApiProperty({description: 'User email'})
     @IsEmail()
     email: string;
 
+    @ApiProperty({description: 'User password'})
     @IsOptional()
     @IsString()
     password?: string;
 
+    @ApiProperty({description: 'User googleId'})
     @IsOptional()
     @IsString()
     googleId?: string;
