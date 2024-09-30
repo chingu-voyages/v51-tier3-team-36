c8e8cb5ceabb3b3d7a0994695ca1800c52fdfa69
Richard Zhang <richardzhiyuanzhang@gmail.com>
2024-09-30 14:38:45 -0400

    create group schema

---
+++ backend/src/groups/schemas/group.schema.ts
@@ -0,0 +1,44 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types, HydratedDocument } from 'mongoose';
+
+export type GroupDocument = HydratedDocument<Group> & {};
+
+@Schema()
+export class Group {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  inviteCode: string;
+
+  @Prop()
+  description?: string;
+
+  @Prop({ required: true, default: 0 })
+  budget: number;
+
+  @Prop({
+    type: [
+      {
+        userId: { type: Types.ObjectId, ref: 'User', required: true },
+        contributionWeight: { type: Number, default: 0 }, // default 0 for equal contribution
+      },
+    ],
+    default: [],
+  })
+  participants: { userId: Types.ObjectId; contributionWeight: number }[];
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Expense' }], default: [] })
+  expenses: Types.ObjectId[];
+}
+
+export const GroupSchema = SchemaFactory.createForClass(Group);
+
+GroupSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  },
+});
