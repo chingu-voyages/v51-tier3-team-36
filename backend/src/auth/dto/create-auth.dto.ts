95d67b2945b76d572ba1035295e59bc74bef95a8
Hayvid <davidhyppolitedh@gmail.com>
2024-10-04 12:19:33 -0400

    extends create user

--- backend/src/auth/dto/create-auth.dto.ts
+++ backend/src/auth/dto/create-auth.dto.ts
@@ -1,22 +1,17 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Matches } from 'class-validator';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
-export class RegisterDto {
-  @ApiProperty({description: 'User name'})
-  @IsNotEmpty()
-  readonly name: string;
-
-  @ApiProperty({description: 'User email'})
-  @IsEmail()
-  readonly email: string;
+export class RegisterDto extends CreateUserDto{
 
   @ApiProperty({description: 'User password'})
+  @IsString()
   @IsNotEmpty()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
   @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).+$/, {
     message:
       'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*)',
   })
-  readonly password: string;
+  password: string;
 }
