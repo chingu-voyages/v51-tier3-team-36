66cfcbef0c9fe9058cfad16e55eee020b97fe9e2
Hayvid <davidhyppolitedh@gmail.com>
2024-10-04 12:01:49 -0400

    Added password validation

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString, IsEmail, IsOptional } from "class-validator";
+import { IsString, IsEmail, IsOptional, MinLength, Matches } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty({description: 'User name'})
@@ -13,6 +13,11 @@ export class CreateUserDto {
     @ApiProperty({description: 'User password'})
     @IsOptional()
     @IsString()
+    @MinLength(8, { message: 'Password must be at least 8 characters long' })
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).+$/, {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*)',
+    })
     password?: string;
 
     @ApiProperty({description: 'User googleId'})
